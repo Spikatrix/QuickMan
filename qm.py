1f948608f9f78fcc1dcb7593ddde74a9a235a9dd
Spikatrix <12792882+Spikatrix@users.noreply.github.com>
2020-07-05 20:28:23 +0530

    Bug fixes, shortened code
    Improving my python and vim fu. Note that this commit includes fixes for
    several awk commands but this means tar's -T no longer work as
    expected. There's no way to do everything so I went for the next best
    option.

--- qm.py
+++ qm.py
@@ -37,13 +37,9 @@ def get_args():
         if arg.startswith("--") and len(arg) > 3:
             actual_args.append(arg[2:])
         elif arg[0] == '-':
-            sub_arg_list = []
-            for sub_arg in arg[1:]:
-                if ord(sub_arg) not in range(65, 91) and ord(sub_arg) not in range(97, 123):
-                    sub_arg_list.clear()
-                    break
-                sub_arg_list.append(sub_arg)
-            actual_args += sub_arg_list
+            sub_arg_list = [sub_arg for sub_arg in arg[1:] if sub_arg.isalpha()]
+            if len(sub_arg_list) == len(arg) - 1:
+                actual_args += sub_arg_list
     return actual_args
 
 def check_flag_option(man_output, line_indices):
@@ -59,7 +55,11 @@ def check_flag_option(man_output, line_indices):
                     continue
                 elif man_arg[0] != '-':
                     break
-                if (len(arg) == 1 and arg == man_arg[1:]) or (len(arg) != 1 and man_arg[2:].startswith(arg)):
+                if ((len(arg) == 1 and arg == man_arg[1:]) or
+                    (len(arg) != 1 and man_arg[2:].startswith(arg)
+                        and (len(man_arg) <= (len(arg) + 2)
+                        or not man_arg[len(arg) + 2].isalpha()
+                        and man_arg[len(arg) + 2] != '-'))):
                     print_flag_option(man_output, index, first_flag_option)
                     first_flag_option = False
                     next_arg = True
@@ -69,9 +69,9 @@ def check_flag_option(man_output, line_indices):
 
 def get_flag_option_indices(man_output):
     line_indices = []
-    for line_index, man_output in enumerate(man_output):
-        stripped_line = man_output.strip()
-        if stripped_line and stripped_line[0] == '-' and (line_index - 1) not in line_indices:
+    for line_index, man_line in enumerate(man_output):
+        stripped_line = man_line.strip()
+        if stripped_line and stripped_line[0] == '-':
             line_indices.append(line_index)
 
     return line_indices
@@ -83,7 +83,8 @@ def main():
 
     command = sys.argv[1]
 
-    man_command_output = subprocess.run(['env', 'COLUMNS=32767', 'man', command], stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+    man_command_output = subprocess.run(['env', 'COLUMNS=32767', 'man', command],
+            stdout=subprocess.PIPE, stderr=subprocess.PIPE)
     man_output = man_command_output.stdout.decode('utf-8').strip().split("\n")
     man_error = man_command_output.stderr.decode('utf-8').strip()
     if man_error.startswith("No manual"):
